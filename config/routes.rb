12e9defacac82553f073a16b0fb70efce2c026a8
ekk7 <kofman.emma@gmail.com>
2020-12-15 23:02:33 -0800

    google cal api

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   resources :symptoms, except: [:index]
+  devise_for :users, :controllers => {:omniauth_callbacks => "callbacks"}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_16_051854) do
+ActiveRecord::Schema.define(version: 2020_12_16_064800) do
 
   create_table "symptoms", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,22 @@ ActiveRecord::Schema.define(version: 2020_12_16_051854) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.decimal "rating"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.string "access_token"
+    t.datetime "expires_at"
+    t.string "refresh_token"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
